6fb8c6c64f91dc0772e5b003eda377d115d3c40a
Alex <alexz2002429@gmail.com>
2021-01-16 18:23:04 -0500

    added youtube caption scraping

--- popup.js
+++ popup.js
@@ -1,42 +1,58 @@
-var saveNote = document.querySelector('#save-note');
-var checkFacts = document.querySelector('#check-facts');
-var deleteNotes = document.querySelector('#delete-notes');
-var notesField = document.querySelector('#note-value');
+var getSubtitles = require("youtube-captions-scraper").getSubtitles;
 
+var saveNote = document.querySelector("#save-note");
+var checkFacts = document.querySelector("#check-facts");
+var deleteNotes = document.querySelector("#delete-notes");
+var notesField = document.querySelector("#note-value");
 
 // Populate Notes From Page
-chrome.tabs.query({
-  active: true,
-  lastFocusedWindow: true
-}, tabs => {
-  let url = tabs[0].url;
-
-})
 
 const json_obj = {
-    1 : {
-      timestamp: 50,
-      statement: "The sky is light"
+  1: {
+    timestamp: 50,
+    statement: "The sky is light",
+  },
+  2: {
+    timestamp: 75,
+    statement: "The Earth is flat",
+  },
+};
+
+analyze.onclick = function add_flagged() {
+  // alert("hi");
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
     },
-    2:{
-      timestamp: 75,
-      statement: "The Earth is flat"
-    },
-}
+    (tabs) => {
+      let url = tabs[0].url;
+      if (!url.includes("youtube.com")) return;
+      var begin = tabs[0].url.indexOf("v=");
+      if (begin == -1) return;
+      var end = tabs[0].url.indexOf("&", begin);
+      alert(url.substring(begin, end));
+      getSubtitles({
+        videoID: url.substring(begin+2, end), // youtube video id
+        lang: "en", // default: `en`
+      }).then(function (captions) {
+        console.log(
+          captions.reduce((map, next) => {
+            map[next["start"]] = next["text"].replaceAll("\n", " ");
+            return map;
+          }, {})
+        );
+      });
+    }
+  );
+  // var progress_bar = document.getElementsByClassName("ytp-progress-bar");
 
-analyze.onclick = function add_flagged(){
-  alert(window.location.url)
-
-  var progress_bar = document.getElementsByClassName("ytp-progress-bar")
-
-
-  for (var keyVal in json_obj){
+  for (var keyVal in json_obj) {
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode(keyVal))
+    li.appendChild(document.createTextNode(keyVal));
     var myFlag = document.createElement("div");
-
-  } 
-}
+  }
+};
 
 ///////////////////////////////////////////////////////////////////////////
 //   // Grab the notes for the page
